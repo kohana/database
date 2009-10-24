1b1b9459a41d9b9c0df17f1d70304de93a3562eb
Chris Bandy <cbandy@jbandy.com>
2009-10-24 11:29:39 -0500

    Add SQL types metadata

--- classes/kohana/database.php
+++ classes/kohana/database.php
@@ -16,6 +16,63 @@ abstract class Kohana_Database {
 	const DELETE =  4;
 
 	/**
+	 * @var array   SQL standard types
+	 */
+	protected static $_types = array(
+		// SQL-92
+		'bit'                   => array('type' => 'string', 'exact' => TRUE),
+		'bit varying'           => array('type' => 'string'),
+		'char'                  => array('type' => 'string', 'exact' => TRUE),
+		'char varying'          => array('type' => 'string'),
+		'character'             => array('type' => 'string', 'exact' => TRUE),
+		'character varying'     => array('type' => 'string'),
+		'date'                  => array('type' => 'string'),
+		'dec'                   => array('type' => 'float', 'exact' => TRUE),
+		'decimal'               => array('type' => 'float', 'exact' => TRUE),
+		'double precision'      => array('type' => 'float'),
+		'float'                 => array('type' => 'float'),
+		'int'                   => array('type' => 'int', 'min' => '-2147483648', 'max' => '2147483647'),
+		'integer'               => array('type' => 'int', 'min' => '-2147483648', 'max' => '2147483647'),
+		'interval'              => array('type' => 'string'),
+		'national char'         => array('type' => 'string', 'exact' => TRUE),
+		'national char varying' => array('type' => 'string'),
+		'national character'    => array('type' => 'string', 'exact' => TRUE),
+		'national character varying' => array('type' => 'string'),
+		'nchar'                 => array('type' => 'string', 'exact' => TRUE),
+		'nchar varying'         => array('type' => 'string'),
+		'numeric'               => array('type' => 'float', 'exact' => TRUE),
+		'real'                  => array('type' => 'float'),
+		'smallint'              => array('type' => 'int', 'min' => '-32768', 'max' => '32767'),
+		'time'                  => array('type' => 'string'),
+		'time with time zone'   => array('type' => 'string'),
+		'timestamp'             => array('type' => 'string'),
+		'timestamp with time zone' => array('type' => 'string'),
+		'varchar'               => array('type' => 'string'),
+
+		// SQL:1999
+		//'array','ref','row'
+		'binary large object'   => array('type' => 'string', 'binary' => TRUE),
+		'blob'                  => array('type' => 'string', 'binary' => TRUE),
+		'boolean'               => array('type' => 'bool'),
+		'char large object'     => array('type' => 'string'),
+		'character large object' => array('type' => 'string'),
+		'clob'                  => array('type' => 'string'),
+		'national character large object' => array('type' => 'string'),
+		'nchar large object'    => array('type' => 'string'),
+		'nclob'                 => array('type' => 'string'),
+		'time without time zone' => array('type' => 'string'),
+		'timestamp without time zone' => array('type' => 'string'),
+
+		// SQL:2003
+		'bigint'    => array('type' => 'int', 'min' => '-9223372036854775808', 'max' => '9223372036854775807'),
+
+		// SQL:2008
+		'binary'            => array('type' => 'string', 'binary' => TRUE, 'exact' => TRUE),
+		'binary varying'    => array('type' => 'string', 'binary' => TRUE),
+		'varbinary'         => array('type' => 'string', 'binary' => TRUE),
+	);
+
+	/**
 	 * @var  array  Database instances
 	 */
 	public static $instances = array();
