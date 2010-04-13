7a288b29e6a0be841b41cfc118ce77a57687ce76
Woody Gilk <woody.gilk@kohanaphp.com>
2010-04-13 09:19:12 +0200

    Databse_MySQL::disconnect does not return the proper boolean when an error is triggered, fixes #2799

--- classes/kohana/database/mysql.php
+++ classes/kohana/database/mysql.php
@@ -115,7 +115,7 @@ class Kohana_Database_MySQL extends Database {
 		catch (Exception $e)
 		{
 			// Database is probably not disconnected
-			$status = is_resource($this->_connection);
+			$status = ! is_resource($this->_connection);
 		}
 
 		return $status;
