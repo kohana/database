31863e7931d4ecaf84e920ded72fdb5d8665921d
Woody Gilk <woody.gilk@kohanaphp.com>
2010-06-06 08:27:24 -0500

    What is that doing in there?

--- classes/kohana/database/query/builder.php
+++ classes/kohana/database/query/builder.php
@@ -108,14 +108,8 @@ abstract class Kohana_Database_Query_Builder extends Database_Query {
 						$value = $db->quote($value);
 					}
 
-					if ($op)
-					{
-						// Make the operator uppercase and spaced
-						$op = ' '.strtoupper($op);
-					}
-
 					// Append the statement to the query
-					$sql .= $db->quote_identifier($column).$op.' '.$value;
+					$sql .= $db->quote_identifier($column).' '.$op.' '.$value;
 				}
 
 				$last_condition = $condition;
