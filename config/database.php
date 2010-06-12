eccee9a4dbc23b27002464412fcbe3579630ff49
Woody Gilk <woody.gilk@kohanaphp.com>
2010-06-12 09:18:40 -0500

    Follow up to f52e71f0f84a0a0d56f3d182c154539262115875, add a comment about the PDO identifier option to the config comments

--- config/database.php
+++ config/database.php
@@ -45,6 +45,11 @@ return array
 			'password'   => 'r00tdb',
 			'persistent' => FALSE,
 		),
+		/**
+		 * The following extra options are available for PDO:
+		 *
+		 * string   identifier  set the escaping identifier
+		 */
 		'table_prefix' => '',
 		'charset'      => 'utf8',
 		'caching'      => FALSE,
