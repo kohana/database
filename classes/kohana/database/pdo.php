afaf8d8d3130c25554af94d8eff1b6c1b49c3fbc
David VanScott <david.vanscott@gmail.com>
2010-07-08 12:56:57 -0700

    should be add_prefix not append

--- classes/kohana/database/pdo.php
+++ classes/kohana/database/pdo.php
@@ -153,7 +153,7 @@ class Kohana_Database_PDO extends Database {
 			array(':method' => __FUNCTION__, ':class' => __CLASS__));
 	}
 
-	public function list_columns($table, $like = NULL, $append_prefix = TRUE)
+	public function list_columns($table, $like = NULL, $add_prefix = TRUE)
 	{
 		throw new Kohana_Exception('Database method :method is not supported by :class',
 			array(':method' => __FUNCTION__, ':class' => __CLASS__));
