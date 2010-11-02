0762d67d314b1480395aece7121f5feefbce4ffe
Isaiah DeRose-Wilson <isaiah@iitechs.com>
2010-11-02 17:23:02 -0400

    Transaction isolation level for MySQL

--- classes/kohana/database/mysql.php
+++ classes/kohana/database/mysql.php
@@ -256,14 +256,34 @@ class Kohana_Database_MySQL extends Database {
 		return parent::datatype($type);
 	}
 
-	public function begin()
+	/**
+	 * Start a SQL transaction
+	 *
+	 * @link http://dev.mysql.com/doc/refman/5.0/en/set-transaction.html
+	 *
+	 * @param string Isolation level
+	 * @return boolean
+	 */
+	public function begin($mode = NULL)
 	{
 		// Make sure the database is connected
 		$this->_connection or $this->connect();
 
+		if ($mode AND ! mysql_query("SET TRANSACTION ISOLATION LEVEL $mode", $this->_connection))
+		{
+			throw new Database_Exception(':error', array(':error' => mysql_error($this->_connection)),
+										 mysql_errno($this->_connection));
+		}
+
 		return (bool) mysql_query('START TRANSACTION', $this->_connection);
 	}
 
+	/**
+	 * Commit a SQL transaction
+	 *
+	 * @param string Isolation level
+	 * @return boolean
+	 */
 	public function commit()
 	{
 		// Make sure the database is connected
@@ -272,6 +292,12 @@ class Kohana_Database_MySQL extends Database {
 		return (bool) mysql_query('COMMIT', $this->_connection);
 	}
 
+	/**
+	 * Rollback a SQL transaction
+	 *
+	 * @param string Isolation level
+	 * @return boolean
+	 */
 	public function rollback()
 	{
 		// Make sure the database is connected
