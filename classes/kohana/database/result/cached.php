kohana/database

commit e34d1eec75e8551a25949d8ef2e03c2c7fb131df

Related to e07467091fc96f3d3cc9d993c599e6bde2fcdc42, FALSE should be returned by Result_Cached::current rather than NULL for compatibility with MySQL_Result

classes/kohana/database/result/cached.php
@@ -45,7 +45,7 @@ class Kohana_Database_Result_Cached extends Database_Result {
 	public function current()
 	{
 		// Return an array of the row
-		return $this->valid() ? $this->_result[$this->_current_row] : NULL;
+		return $this->valid() ? $this->_result[$this->_current_row] : FALSE;
 	}
 
 } // End Database_Result_Cached

commit 1e7b45c4d1a6d01b2f7f99f2571f2a5b51bfc71a

Remove incorrect compatibility behavior in Result_Cached that was introduced by e34d1eec75e8551a25949d8ef2e03c2c7fb131df

classes/kohana/database/result/cached.php
@@ -45,7 +45,7 @@ class Kohana_Database_Result_Cached extends Database_Result {
 	public function current()
 	{
 		// Return an array of the row
-		return $this->valid() ? $this->_result[$this->_current_row] : FALSE;
+		return $this->valid() ? $this->_result[$this->_current_row] : NULL;
 	}
 
 } // End Database_Result_Cached