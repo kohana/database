6465093bacc386b40356abf61a78e34fa390d7d5
Jason Zdan <jason@frontlogic.com>
2013-07-31 10:37:44 -0600

    Coding standards fixes

--- classes/Kohana/Database/Query/Builder/Select.php
+++ classes/Kohana/Database/Query/Builder/Select.php
@@ -309,7 +309,7 @@ class Kohana_Database_Query_Builder_Select extends Database_Query_Builder_Where 
 	 */
 	public function offset($number)
 	{
-		$this->_offset = $number === null ? null : (int) $number;
+		$this->_offset = ($number === NULL) ? NULL : (int) $number;
 
 		return $this;
 	}
