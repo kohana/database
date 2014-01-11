be193d14a87e53acbe0fa69600a118a0970d0c1e
biakaveron <biakaveron@gmail.com>
2014-01-12 00:07:19 +0400

    handle both <> NULL and != NULL as IS NOT NULL

--- classes/kohana/database/query/builder.php
+++ classes/kohana/database/query/builder.php
@@ -80,7 +80,7 @@ abstract class Kohana_Database_Query_Builder extends Database_Query {
 							// Convert "val = NULL" to "val IS NULL"
 							$op = 'IS';
 						}
-						elseif ($op === '!=')
+						elseif ($op === '!=' OR $op === '<>')
 						{
 							// Convert "val != NULL" to "valu IS NOT NULL"
 							$op = 'IS NOT';
