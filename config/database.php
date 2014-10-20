6e883e352a0598b00c230efca134206a7abf6629
Gustavo Novaro <gnovaro@gmail.com>
2014-10-20 11:42:55 -0300

    Add default database charset to mysql dsn connection
    Set by default in utf8

--- config/database.php
+++ config/database.php
@@ -39,7 +39,7 @@ return array
 			 * string   password    database password
 			 * boolean  persistent  use persistent connections?
 			 */
-			'dsn'        => 'mysql:host=localhost;dbname=kohana',
+			'dsn'        => 'mysql:host=localhost;dbname=kohana;charset=utf8',
 			'username'   => 'root',
 			'password'   => 'r00tdb',
 			'persistent' => FALSE,
